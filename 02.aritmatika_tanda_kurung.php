25b1328d183e5647f708fcfcec633d967ed2e048
Feekz <150095009+Safrust@users.noreply.github.com>
2024-09-26 18:59:54 +0700

    Create 02.aritmatika_tanda_kurung.php

---
+++ 02.aritmatika_tanda_kurung.php
@@ -0,0 +1,12 @@
+<?php
+    $hasil1 = 3 + 8/5 - 3*9+9 ;
+
+    var_dump($hasil1);  // float(-13.4)
+
+    echo "<br>";
+
+    $hasil2= ((3 + 8) / (5 - 3)) * (9+9) ;
+
+    var_dump($hasil2);  // float(99)
+
+?>
