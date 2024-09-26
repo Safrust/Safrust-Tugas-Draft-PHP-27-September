8b5cdbfc3732e20ece2d560d6d47b33b09f528bb
Feekz <150095009+Safrust@users.noreply.github.com>
2024-09-26 19:23:41 +0700

    Create 03a.aritmatika_bukan_angka_warning.php

---
+++ 03a.aritmatika_bukan_angka_warning.php
@@ -0,0 +1,10 @@
+<?php
+// PHP Warning: A non-numeric value encountered
+$hasil = "5 ekor sapi" + "3 ekor kambing";
+var_dump($hasil); echo "<br>";      // int(8)
+
+// PHP Warning: A non-numeric value encountered
+$hasil = "17.5cm" + "1.99cm";
+var_dump($hasil); echo "<br>";      // float(19.49)
+
+?>
